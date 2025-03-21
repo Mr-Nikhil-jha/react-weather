9c65f467ccc589252608ea3926baef1dfb38e9a7
Vivek Rai <96563624+vivekrai1999@users.noreply.github.com>
2025-03-21 19:05:40 +0530

    Refractor: GetWeatherData to use async/await and add error handling

--- src/ApiCall.js
+++ src/ApiCall.js
@@ -1,10 +1,13 @@
-let apiKey = import.meta.env.VITE_API_KEY;
-
-function GetWeatherData({ city, setData }) {
-  let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
-  fetch(url)
-    .then((response) => response.json())
-    .then((result) => setData(result));
-}
+const GetWeatherData = async ({ city, setData }) => {
+    let apiKey = import.meta.env.VITE_API_KEY;
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
+    try {
+        const res = await fetch(url);
+        const data = await res.json();
+        setData(data);
+    } catch (err) {
+        console.error(err, "Unable to fetch data");
+    }
+};
 
 export { GetWeatherData };
